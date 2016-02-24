5e54756ce5e511c9a4f36cb3265bdfce6fb9f356
bradhilton <brad.hilton.nw@gmail.com>
2016-02-23 19:00:38 -0700

    Initial Commit

---
+++ SwiftCallbacks/SwiftCallbacks.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCallbacks.h
+//  SwiftCallbacks
+//
+//  Created by Bradley Hilton on 2/23/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCallbacks.
+FOUNDATION_EXPORT double SwiftCallbacksVersionNumber;
+
+//! Project version string for SwiftCallbacks.
+FOUNDATION_EXPORT const unsigned char SwiftCallbacksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCallbacks/PublicHeader.h>
+
+
